a478d64837eea57c263a4378f7309519c07a6a3a
iamluc <luc@vieillescazes.net>
2015-11-24 17:33:31 +0100

    Fix crash with boot2docker

--- symfony/entrypoint.sh
+++ symfony/entrypoint.sh
@@ -3,8 +3,8 @@ set -e
 
 # Change www-data's uid & guid to be the same as directory in host
 # Fix cache problems
-usermod -u `stat -c %u /var/www/html` www-data
-groupmod -g `stat -c %g /var/www/html` www-data
+usermod -u `stat -c %u /var/www/html` www-data || true
+groupmod -g `stat -c %g /var/www/html` www-data || true
 
 if [ "$1" = 'apache2ctl' ]; then
     # let's start as root
