fd4556a4ada1184a5594115fee8b711cb49d62fe
Benjamin Lazarecki <benjamin.lazarecki@sensiolabs.com>
2015-12-01 16:09:20 +0100

    Allow to configure www-data uid & guid

--- symfony/entrypoint.sh
+++ symfony/entrypoint.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -e
 
+: ${WWW_DATA_UID:=`stat -c %u /var/www/html`}
+: ${WWW_DATA_GUID:=`stat -c %g /var/www/html`}
+
 # Change www-data's uid & guid to be the same as directory in host
 # Fix cache problems
-usermod -u `stat -c %u /var/www/html` www-data || true
-groupmod -g `stat -c %g /var/www/html` www-data &> /dev/null || true
+if [ "`id -u www-data`" != "$WWW_DATA_UID" ]; then
+    usermod -u $WWW_DATA_UID www-data || true
+fi
+
+if [ "`id -u www-data`" != "$WWW_DATA_GUID" ]; then
+    groupmod -g $WWW_DATA_GUID www-data || true
+fi
 
 if [ "$1" = 'apache2ctl' ]; then
     # let's start as root
