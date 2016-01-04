1fbae4f726ad5d6c4bc5b5b9d8489cb726de168a
Benjamin Lazarecki <benjamin.lazarecki@sensiolabs.com>
2016-01-04 20:28:07 +0100

    Add explicit error message

--- symfony/entrypoint.sh
+++ symfony/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 : ${WWW_DATA_UID:=`stat -c %u /var/www/html`}
 : ${WWW_DATA_GUID:=`stat -c %g /var/www/html`}
@@ -7,11 +6,19 @@ set -e
 # Change www-data's uid & guid to be the same as directory in host
 # Fix cache problems
 if [ "`id -u www-data`" != "$WWW_DATA_UID" ]; then
-    usermod -u $WWW_DATA_UID www-data || true
+    usermod -u $WWW_DATA_UID www-data > /dev/null 2>&1
+    if [ $? != 0 ]; then
+        echo "The UID $WWW_DATA_UID is already used by another user."
+        exit 1
+    fi
 fi
 
 if [ "`id -u www-data`" != "$WWW_DATA_GUID" ]; then
-    groupmod -g $WWW_DATA_GUID www-data || true
+    groupmod -g $WWW_DATA_GUID www-data > /dev/null 2>&1
+    if [ $? != 0 ]; then
+        echo "The GUID $WWW_DATA_GUID is already used by another group."
+        exit 1
+    fi
 fi
 
 if [ "$1" = 'apache2ctl' ]; then
